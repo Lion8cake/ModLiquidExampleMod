4d53665008693c2599d96594258f9e64eb6c00c0
Lion8cake <Lion8cake@hotmail.com>
2026-01-05 17:12:51 +1100

    Example Grate and IgnoresWater "showcase"

---
+++ Content/Items/ExampleGrate.cs
@@ -0,0 +1,31 @@
+﻿using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ModLiquidExampleMod.Content.Items
+{
+	public class ExampleGrate : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+			ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.Grate;
+			ItemID.Sets.ShimmerTransformToItem[ItemID.Grate] = Type;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.createTile = ModContent.TileType<Tiles.ExampleGrate>();
+			Item.width = 16;
+			Item.height = 16;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTurn = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.autoReuse = true;
+			Item.maxStack = Item.CommonMaxStack;
+			Item.consumable = true;
+		}
+	}
+}
